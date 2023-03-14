e1c0400f9cb7ef93ae9835a935b9d4030c6e6b43
Eduardo Ramirez <eduardoramirezdev@outlook.com>
2023-03-14 17:32:29 -0500

    Enums for two Aprendiz Class fields

---
+++ rest-api/src/main/java/tech/edev404/restapi/model/entities/enums/TipoDocumento.java
@@ -0,0 +1,8 @@
+package tech.edev404.restapi.model.entities.enums;
+
+public enum TipoDocumento {
+    CC,
+    TI,
+    PPT,
+    PEP
+}
---
+++ rest-api/src/main/java/tech/edev404/restapi/model/entities/status/AprendizStatus.java
@@ -0,0 +1,6 @@
+package tech.edev404.restapi.model.entities.status;
+
+public enum AprendizStatus {
+    HABILITADO,
+    INHABILITADO
+}
